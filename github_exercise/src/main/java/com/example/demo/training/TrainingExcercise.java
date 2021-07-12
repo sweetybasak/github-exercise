c947f94614c23eb802106554c8b7377b3fd1c86e
Admin <Admin@DESKTOP-F8V312A>
2021-07-12 15:18:59 +0530

    REQ-102 new code added for customer info

---
+++ github_exercise/src/main/java/com/example/demo/training/TrainingExcercise.java
@@ -0,0 +1,16 @@
+package com.example.demo.training;
+
+public class TrainingExcercise {
+
+	public void customerinfo() {
+		
+		String customerName;
+		int phoneNumber;
+	}
+
+	public TrainingExcercise() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+}
