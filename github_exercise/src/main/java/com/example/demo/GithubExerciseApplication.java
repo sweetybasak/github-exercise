8ba88f9196cfe6857cb903c77c1d3d16c90cb6b6
sweetybasak <sweetybasak221@gmail.com>
2021-07-12 13:20:07 +0530

    github-exercise

---
+++ github_exercise/src/main/java/com/example/demo/GithubExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubExerciseApplication.class, args);
+	}
+
+}
